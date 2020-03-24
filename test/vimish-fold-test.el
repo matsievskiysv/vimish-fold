1719537ecdc8f0f3b07cb0692ffb0d18e06b3949
Matsievskiy S.V <matsievskiysv@gmail.com>
2020-03-24 23:50:41 +0300

    Add the ‘vimish-fold-from-marks’ function

--- test/vimish-fold-test.el
+++ test/vimish-fold-test.el
@@ -30,22 +30,45 @@
 (require 'cl-lib)
 (require 'vimish-fold)
 
-(defmacro with-test-file (&rest body)
-  "Run BODY in a temporary buffer with test file loaded."
+(defmacro with-test-file (file &rest body)
+  "Run BODY in a temporary buffer with test FILE loaded."
+  (declare (indent 1))
   `(with-temp-buffer
-     (insert-file-contents "test/test.txt")
+     (insert-file-contents ,file)
      ,@body))
 
-(ert-deftest vimish-fold-vimish-fold/successful ()
-  (with-test-file
-   (vimish-fold 666 1324)
-   (let* ((os (vimish-fold--folds-in (point-min) (point-max)))
-          (o  (car os)))
-     (should-not (null o))
-     (should (eq (overlay-get o 'type)
-                 'vimish-fold--folded))
-     (should (= (overlay-start o) 635))
-     (should (= (overlay-end   o) 1323)))))
+(ert-deftest vimish-fold-vimish-fold/test-fold ()
+  "Test `vimish-fold' function."
+  (with-test-file "test/fold.txt"
+    (vimish-fold 666 1324)
+    (let* ((os (vimish-fold--folds-in (point-min) (point-max)))
+           (o  (car os)))
+      (should-not (null o))
+      (should (eq (overlay-get o 'type)
+                  'vimish-fold--folded))
+      (should (= (overlay-start o) 635))
+      (should (= (overlay-end   o) 1323)))))
+
+(ert-deftest vimish-fold-vimish-fold/test-marks ()
+  "Test `vimish-fold-from-marks' function."
+  (with-test-file "test/marks.py"
+    (setq vimish-fold-marks '("{{{" . "}}}"))
+    (vimish-fold-delete-all)
+    (vimish-fold-from-marks)
+    (should (equal (cl-sort (cl-mapcar 'overlay-start (vimish-fold--folds-in (point-min) (point-max))) '<) '(24 59)))
+    (should (equal (cl-sort (cl-mapcar 'overlay-end (vimish-fold--folds-in (point-min) (point-max))) '<) '(57 114)))
+    (should (cl-reduce '(lambda (x o) (and (eq (overlay-get o 'type) 'vimish-fold--unfolded) x)) (vimish-fold--folds-in (point-min) (point-max)) :initial-value t))
+    (vimish-fold-refold-all)
+    (should (cl-reduce '(lambda (x o) (and (eq (overlay-get o 'type) 'vimish-fold--folded) x)) (vimish-fold--folds-in (point-min) (point-max)) :initial-value t))
+    (setq vimish-fold-marks '("<<<" . ">>>"))
+    (vimish-fold-delete-all)
+    (vimish-fold-from-marks)
+    (should (equal (cl-sort (cl-mapcar 'overlay-start (vimish-fold--folds-in (point-min) (point-max))) '<) '(116)))
+    (should (equal (cl-sort (cl-mapcar 'overlay-end (vimish-fold--folds-in (point-min) (point-max))) '<) '(267)))
+    (should (cl-reduce '(lambda (x o) (and (eq (overlay-get o 'type) 'vimish-fold--unfolded) x)) (vimish-fold--folds-in (point-min) (point-max)) :initial-value t))
+    (vimish-fold-refold-all)
+    (should (cl-reduce '(lambda (x o) (and (eq (overlay-get o 'type) 'vimish-fold--folded) x)) (vimish-fold--folds-in (point-min) (point-max)) :initial-value t))
+    ))
 
 (provide 'vimish-fold-test)
 
