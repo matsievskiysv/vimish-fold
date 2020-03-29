63685239655a151181b9152e45478dad587f86f2
Mark Karpov <markkarpov92@gmail.com>
2020-03-29 14:42:58 +0200

    Version bump (0.3.0)

--- vimish-fold.el
+++ vimish-fold.el
@@ -6,7 +6,7 @@
 ;; Author: Mark Karpov <markkarpov92@gmail.com>
 ;; Author: Magnar Sveen <magnars@gmail.com>
 ;; URL: https://github.com/mrkkrp/vimish-fold
-;; Version: 0.2.3
+;; Version: 0.3.0
 ;; Package-Requires: ((emacs "24.4") (cl-lib "0.5") (f "0.18.0"))
 ;; Keywords: convenience
 ;;
